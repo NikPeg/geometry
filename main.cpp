ab6f194631108edee0eb842844f861d69ac454e0
nspeganov <nspeganov@yandex-team.ru>
2021-10-03 18:34:09 +0300

    Add basics in Circle

--- main.cpp
+++ main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 
-class Shape {
+class Figure {
 public:
     virtual double perimeter();
-    bool operator<(Shape other) {
+    bool operator<(Figure other) {
         return perimeter() < other.perimeter();
     }
-    bool operator==(Shape other) {
+    bool operator==(Figure other) {
         return perimeter() == other.perimeter();
     }
+};
+
+class Point {
+public:
+    int x, y;
+    Point() {
+        x = 0;
+        y = 0;
+    }
+    Point(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+};
+
+class Circle: virtual public Figure {
+public:
+    Point centre;
+    double radius;
+    Circle(Point _centre, double _radius) {
+        centre = _centre;
+        radius = _radius;
+    }
+
 };
 
 int main() {
