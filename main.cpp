162121aa647fabe8d7efd99c1b2c550f3b20f158
nspeganov <nspeganov@yandex-team.ru>
2021-10-03 19:28:54 +0300

    Add color

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 #include <math.h>
 #include <iostream>
 
+enum Color{
+    red,
+    orange,
+    yellow,
+    green,
+    light_blue,
+    blue,
+    violet
+};
+
 class Figure {
 public:
+    Color color;
     virtual double perimeter();
     bool operator<(Figure other) {
         return perimeter() < other.perimeter();
@@ -33,9 +44,10 @@ class Circle: virtual public Figure {
 public:
     Point centre;
     double radius;
-    Circle(Point _centre, double _radius) {
+    Circle(Point _centre, double _radius, Color _color) {
         centre = _centre;
         radius = _radius;
+        color = _color;
     }
     double perimeter() override {
         return 2 * M_PI * radius;
@@ -45,9 +57,10 @@ public:
 class Rectangle: virtual public Figure {
 public:
     Point left_up, right_down;
-    Rectangle(Point _left_up, Point _right_down) {
+    Rectangle(Point _left_up, Point _right_down, Color _color) {
         left_up = _left_up;
         right_down = _right_down;
+        color = _color;
     }
     double perimeter() override {
         return 2 * (abs(left_up.x - right_down.x) + abs(left_up.y - right_down.y));
@@ -57,10 +70,11 @@ public:
 class Triangle: virtual public Figure {
 public:
     Point a, b, c;
-    Triangle(Point _a, Point _b, Point _c) {
+    Triangle(Point _a, Point _b, Point _c, Color _color) {
         a = _a;
         b = _b;
         c = _c;
+        color = _color;
     }
     double perimeter() override {
         return Point::distance(a, b) + Point::distance(b, c) + Point::distance(c, a);
