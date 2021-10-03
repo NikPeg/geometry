a1468c05be48dd848f8470c3f9939f0b9b8d528e
nspeganov <nspeganov@yandex-team.ru>
2021-10-03 21:41:30 +0300

    Reading all figures

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <iostream>
 
-enum Color{
+enum Color {
     RED,
     ORANGE,
     YELLOW,
@@ -28,6 +28,8 @@ std::string colorToString(Color color) {
             return "BLUE";
         case VIOLET:
             return "VIOLET";
+        default:
+            return "NOT A COLOR!";
     }
 }
 
@@ -35,7 +37,7 @@ Color stringToColor(std::string color_string) {
     if (color_string == "RED")
         return RED;
     if (color_string == "ORANGE")
-            return ORANGE;
+        return ORANGE;
     if (color_string == "YELLOW")
         return YELLOW;
     if (color_string == "GREEN")
@@ -46,16 +48,19 @@ Color stringToColor(std::string color_string) {
         return BLUE;
     if (color_string == "VIOLET")
         return VIOLET;
+    return RED;
 }
 
 class Figure {
 public:
     Color color;
-    virtual double perimeter();
-    virtual void read();
+
+    virtual double perimeter() {};
+
     bool operator<(Figure other) {
         return perimeter() < other.perimeter();
     }
+
     bool operator==(Figure other) {
         return perimeter() == other.perimeter();
     }
@@ -64,72 +69,109 @@ public:
 class Point {
 public:
     int x, y;
+
     Point() {
         x = 0;
         y = 0;
     }
+
     Point(int _x, int _y) {
         x = _x;
         y = _y;
     }
+
     static double distance(Point a, Point b) {
         return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
     }
 };
 
-class Circle: virtual public Figure {
+class Circle : virtual public Figure {
 public:
     Point centre;
     double radius;
+
     Circle() {
         centre = Point();
         radius = 0;
         color = RED;
     }
+
     Circle(Point _centre, double _radius, Color _color) {
         centre = _centre;
         radius = _radius;
         color = _color;
     }
+
     double perimeter() override {
         return 2 * M_PI * radius;
     }
-    void read() {
+
+    static Circle read() {
         Circle result = Circle();
         std::string color_string;
-        scanf("%d %d %lg %u", &result.centre.x, &result.centre.y, &result.radius, &color_string);
+        scanf("%d %d %lg %s", &result.centre.x, &result.centre.y, &result.radius, &color_string[0]);
         result.color = stringToColor(color_string);
+        return result;
     }
 };
 
-class Rectangle: virtual public Figure {
+class Rectangle : virtual public Figure {
 public:
     Point left_up, right_down;
+
+    Rectangle() {
+        left_up = Point();
+        right_down = Point();
+    }
+
     Rectangle(Point _left_up, Point _right_down, Color _color) {
         left_up = _left_up;
         right_down = _right_down;
         color = _color;
     }
+
     double perimeter() override {
         return 2 * (abs(left_up.x - right_down.x) + abs(left_up.y - right_down.y));
     }
-    void read() {
-        Circle result = Circle();
-        scanf("%d %d %lg %u", &result.centre.x, &result.centre.y, &result.radius, &result.color);
+
+    static Rectangle read() {
+        Rectangle result = Rectangle();
+        std::string color_string;
+        scanf("%d %d %d %d %s", &result.left_up.x, &result.left_up.y, &result.right_down.x, &result.right_down.y,
+              &color_string[0]);
+        result.color = stringToColor(color_string);
+        return result;
     }
 };
 
-class Triangle: virtual public Figure {
+class Triangle : virtual public Figure {
 public:
     Point a, b, c;
+
+    Triangle() {
+        a = Point();
+        b = Point();
+        c = Point();
+    }
+
     Triangle(Point _a, Point _b, Point _c, Color _color) {
         a = _a;
         b = _b;
         c = _c;
         color = _color;
     }
+
     double perimeter() override {
         return Point::distance(a, b) + Point::distance(b, c) + Point::distance(c, a);
+    }
+
+    static Triangle read() {
+        Triangle result = Triangle();
+        std::string color_string;
+        scanf("%d %d %d %d %d %d %s", &result.a.x, &result.a.y, &result.b.x, &result.b.y, &result.c.x, &result.c.y,
+              &color_string[0]);
+        result.color = stringToColor(color_string);
+        return result;
     }
 };
 
@@ -141,9 +183,15 @@ int main(int argc, char *argv[]) {
     Figure figures[figures_count];
     std::string figure_type, color;
     for (int i = 0; i < figures_count; ++i) {
-        scanf("%s", &figure_type);
+        scanf("%s", &figure_type[0]);
         if (figure_type == "Circle") {
-            figures[i] = Circle.read();
+            figures[i] = Circle::read();
+        }
+        else if (figure_type == "Rectangle") {
+            figures[i] = Rectangle::read();
+        }
+        else if (figure_type == "Triangle") {
+            figures[i] = Triangle::read();
         }
     }
 }
